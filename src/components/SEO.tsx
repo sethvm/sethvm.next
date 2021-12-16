1f1a617c3d5aab32c915ba4fe5871fbdcce0e4e9
Seth Morenos <sethvm64@gmail.com>
2021-12-16 01:09:13 -0500

    change theme colour

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -20,7 +20,7 @@ const SEO = ({
             <meta charSet="utf-8" />
             <meta name="viewport" content="width=device-width, initial-scale=1" />
             <meta name="author" content="Seth Morenos" />
-            <meta name="theme-color" content="#004080" />
+            <meta name="theme-color" content="#FAFAFA" />
             <link rel="icon" href="/favicon.png" />
             <meta property="og:image" content={image} />
             <meta property="og:image:width" content="1200" />
