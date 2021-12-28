f11677e74841cde1ec22d098957a53c17d324cb9
Seth Morenos <sethvm64@gmail.com>
2021-12-27 21:43:57 -0500

    filter out react-ga localhost traffic from analytics admin dash

--- src/components/Analytics.ts
+++ src/components/Analytics.ts
@@ -3,35 +3,20 @@ import { useEffect } from 'react'
 import { useRouter } from 'next/router'
 import ReactGA from 'react-ga'
 
-// local environments to disable tracking for
-const localEnvs = [
-    'localhost'
-]
-
 const Analytics = () => {
 
     const trackerID = 'UA-152533938-2';
     const location = useRouter();
-    const host = typeof window !== 'undefined' && window.location.hostname ? window.location.hostname : '';
-    let isLocal = false;
 
-    // checks if build is running on local environment
-    let i = 0;
-    while ((i < localEnvs.length) && (!isLocal)) {
-        if (host === localEnvs[i]) isLocal = true;
-        i++;
-    }
-
-    // initializes ga tracking if non-local environment
+    // initialize ga tracking
     useEffect(() => {
-        if (!isLocal) ReactGA.initialize(trackerID);
-    }, [isLocal]);
+        ReactGA.initialize(trackerID);
+    }, []);
 
-    // updates page_path value
-    // increments pageview count
+    // increment pageview count
     useEffect(() => {
-        if (!isLocal) ReactGA.pageview(location.pathname);
-    }, [isLocal, location.pathname]);
+        ReactGA.pageview(location.pathname);
+    }, [location.pathname]);
 
     return null;
 }
