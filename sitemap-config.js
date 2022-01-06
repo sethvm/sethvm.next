505cd18283bc9c7457447c3fa2280916cda99f62
Seth Morenos <sethvm64@gmail.com>
2022-01-06 13:49:21 -0500

    configure sitemap generator

---
+++ sitemap-config.js
@@ -0,0 +1,31 @@
+module.exports = {
+    siteUrl: 'https://sethvm.com',
+    changefreq: 'monthly',
+    priority: 1,
+    lastmod: new Date().toISOString(),
+
+    additionalPaths: async () => {
+        const result = [];
+
+        result.push(
+            {
+                loc: '/about',
+                priority: 0.8,
+            },
+            {
+                loc: '/ops-pick-history',
+                priority: 0.7,
+            },
+            {
+                loc: '/proximity-transfer',
+                priority: 0.7,
+            },
+            {
+                loc: '/patient-support-app',
+                priority: 0.7,
+            },
+        );
+
+        return result;
+    }
+}
