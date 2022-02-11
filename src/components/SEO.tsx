04ad6c83753b07270d5b8a3f2bf723d75d917302
Seth Morenos <sethvm64@gmail.com>
2022-02-10 22:34:09 -0500

    reconfigure html locale attribute

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -10,7 +10,6 @@ interface Props {
 
 const SEO = ({ title, description, image, url, noCrawl }: Props) => (
     <Head>
-        <html lang='en-CA' />
         <meta charSet='utf-8' />
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <title>{title}</title>
