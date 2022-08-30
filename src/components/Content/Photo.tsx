8f9c0e9f02a5eb3ea557b16f05fc851b71b1fd1e
Seth Morenos <sethvm64@gmail.com>
2022-08-29 21:07:52 -0400

    update about page

---
+++ src/components/Content/Photo.tsx
@@ -0,0 +1,14 @@
+// uncontained photo component
+// horizontal margins line up with text content
+import stylePhoto from './Photo.module.scss';
+
+interface Props {
+    src: string;
+    alt: string;
+}
+
+const Photo = ({ src, alt }: Props) => (
+    <img className={stylePhoto.photo} src={src} alt={alt} />
+);
+
+export default Photo;
