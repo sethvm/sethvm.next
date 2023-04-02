865e5cb745d511ad86fd0880500f47e50817f1a7
Seth Morenos <sethvm64@gmail.com>
2023-04-02 14:13:19 -0400

    install and configure react-ga4

--- src/components/Analytics.ts
+++ src/components/Analytics.ts
@@ -2,19 +2,36 @@
 import { useEffect } from 'react';
 import { useRouter } from 'next/router';
 import ReactGA from 'react-ga';
+import ReactGA4 from 'react-ga4';
 
 const Analytics = () => {
-    const trackerID = 'UA-152533938-2';
+    const uaTrackerID = 'UA-152533938-2';
+    const ga4TrackerID = 'G-L5EQBK0RT0';
     const location = useRouter();
 
     // initialize ga tracking
     useEffect(() => {
-        ReactGA.initialize(trackerID);
+
+        //ua tracker
+        ReactGA.initialize(uaTrackerID);
+
+        //ga4 tracker
+        ReactGA4.initialize(ga4TrackerID);
+
     }, []);
 
     // increment pageview count
     useEffect(() => {
+
+        //ua pageview
         ReactGA.pageview(location.pathname);
+
+        //ga4 pageview
+        ReactGA4.send({
+            hitType: 'pageview',
+            page: location.pathname,
+        });
+
     }, [location.pathname]);
 
     return null;
