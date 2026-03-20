6794e825bae168cc001bc6a6abcde301ac642dd7
Seth Morenos <sethvm64@gmail.com>
2026-03-20 13:01:07 -0400

    update content

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,7 +2,7 @@ import styleFooter from './Footer.module.scss';
 
 const Footer = () => (
     <footer id='footer' className={styleFooter.container}>
-        <div className={styleFooter.text}>© 2025 Seth Morenos</div>
+        <div className={styleFooter.text}>© 2026 Seth Morenos</div>
     </footer>
 );
 
