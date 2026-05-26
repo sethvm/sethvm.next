fa7944bb91db6e33305a5a05f173764fbb3eddf2
Seth Morenos <sethvm64@gmail.com>
2026-05-26 11:04:50 -0400

    update content

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -10,7 +10,7 @@ interface Props {
 
 const SEO = ({
     title = 'Seth Morenos',
-    description = "I'm Seth, a Toronto-based product designer passionate about building experiences that help move the world forward. Currently working to empower small business owners.",
+    description = "I'm Seth, a Toronto-based product designer passionate about building experiences that empower everyone to do more. Currently working on invoicing for small businesses.",
     image = '/preview.png',
     url,
     noCrawl,
