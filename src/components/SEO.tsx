c5b6f655b6d09945c22ec321c094893358812101
Seth Morenos <sethvm64@gmail.com>
2026-06-04 12:07:12 -0400

    update theme-color to match new background colour

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -23,7 +23,7 @@ const SEO = ({
         {url && <link rel='canonical' href={url} />}
         <link rel='icon' href='/favicon.png' />
         <meta name='author' content='Seth Morenos' />
-        <meta name='theme-color' content='#F7F5EC' />
+        <meta name='theme-color' content='#FDFCFA' />
         <meta property='og:title' content={title} />
         {description && <meta property='og:description' content={description} />}
         {url && <meta property='og:url' content={url} />}
