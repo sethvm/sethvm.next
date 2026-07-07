5983794225bad6c87f57ab37ce260b819dd88294
Seth Morenos <sethvm64@gmail.com>
2026-07-07 12:55:44 -0400

    retire BlueBlock.tsx

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,27 +1,16 @@
 import Link from 'next/link';
-import { useRouter } from 'next/router';
 import styleNavBar from './NavBar.module.scss';
 
-const NavBar = () => {
-    const { pathname } = useRouter();
-    // About keeps its right-edge navy stripe behind ABOUT; Home no longer has a navy block,
-    // so its route must be dark (navy) to stay visible on the cream background.
-    const hasBlueBackdrop = pathname === '/about';
-    const routeClass = hasBlueBackdrop
-        ? styleNavBar.route
-        : `${styleNavBar.route} ${styleNavBar.routeDark}`;
-
-    return (
-        <header id='header' className={styleNavBar.navbar} aria-label='navbar'>
-            <Link href='/' className={styleNavBar.brand} aria-label='Seth Morenos'>
-                <span className={styleNavBar.firstName}>SETH</span>&nbsp;
-                <span className={styleNavBar.lastName}>MORENOS</span>
-            </Link>
-            <Link href='/about' className={routeClass} aria-label='About'>
-                ABOUT
-            </Link>
-        </header>
-    );
-};
+const NavBar = () => (
+    <header id='header' className={styleNavBar.navbar} aria-label='navbar'>
+        <Link href='/' className={styleNavBar.brand} aria-label='Seth Morenos'>
+            <span className={styleNavBar.firstName}>SETH</span>&nbsp;
+            <span className={styleNavBar.lastName}>MORENOS</span>
+        </Link>
+        <Link href='/about' className={styleNavBar.route} aria-label='About'>
+            ABOUT
+        </Link>
+    </header>
+);
 
 export default NavBar;
