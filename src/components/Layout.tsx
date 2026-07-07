c6afd5d1b99cf259a81392d9082c34e3e57bb616
Seth Morenos <sethvm64@gmail.com>
2026-07-07 12:55:44 -0400

    retire BlueBlock.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -4,7 +4,6 @@ import Wrapper from '../components/Wrapper/Wrapper';
 import QuickLinks from '../components/QuickLinks/QuickLinks';
 import SideBar from '../components/SideBar/SideBar';
 import Footer from '../components/Footer/Footer';
-import BlueBlock from '../components/BlueBlock/BlueBlock';
 
 interface Props {
     children: React.ReactNode;
@@ -18,7 +17,6 @@ const Layout = ({ children }: Props) => (
         <QuickLinks />
         <SideBar />
         <Footer />
-        <BlueBlock />
     </>
 );
 
