17285565ed87f08657ed7d8187f68422841117b9
Seth Morenos <sethvm64@gmail.com>
2026-07-12 17:06:05 -0400

    update landing page intro

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,11 +19,11 @@ const Home = () => (
                     I'm <span className={homePageStyle.greetingAccent}>Seth.</span>
                 </Heading>
                 <Paragraph className={homePageStyle.introBody}>
-                    Product designer passionate about{' '}
+                    <span className='bold'>Product designer</span> passionate about{' '}
                     <span className='bold'>empowering everyone to do more</span>.
                 </Paragraph>
                 <Paragraph className={homePageStyle.introBody}>
-                    Currently working on invoicing solutions at EverCommerce.
+                    Currently working on invoicing and payments solutions at EverCommerce.
                 </Paragraph>
                 <Paragraph className={homePageStyle.introBody}>
                     Previously at Intuit, Interac and Loblaw Digital.
